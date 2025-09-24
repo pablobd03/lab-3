96d7e68664e0253dedab25c50b9ef9388c65fb34
Pablo Bermúdez <bermudezpablo@gmail.com>
2025-09-23 22:06:58 -0600

    Se añadió funcionalidad para calcular el factorial de un número ingresado por el usuario

--- ejercicio1.c
+++ ejercicio1.c
@@ -10,9 +10,19 @@ int factorial(int n) {
 }
 
 int main(int argc, char *argv[]) {
-    int fac4 = factorial(4);
-    int fac5 = factorial(5);
-    printf("4! = %d, 5! = %d\n", fac4, fac5);
+	int fac4 = factorial(4);
+	int fac5 = factorial(5);
+    	int numero;
+	
+	//Solicita un número entero al usuario
+	printf("Ingrese un número entero: ");
+	scanf("%d", &numero);
+
+	//Se llama a la función factorial y se envía el número ingresado por el usuario como argumento
+	int facn=factorial(numero);
+
+	//Imprime el resultado
+    	printf("4! = %d, 5! = %d, \nEl factorial de su número es: %d! = %d \n", fac4, fac5, numero, facn);
     return 0;
 }
 
