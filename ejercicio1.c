b0ccb46384288a7bcbd1db516cc8ecd013ff31e9
Pablo Bermúdez <bermudezpablo@gmail.com>
2025-09-27 17:44:03 -0600

    Se han realizado cambios menores a comentarios y printf

--- ejercicio1.c
+++ ejercicio1.c
@@ -9,20 +9,13 @@ int factorial(int n) {
     return i;
 }
 
-int main(int argc, char *argv[]) {
+int main (int argc, char *argv[]) {
 	int fac4 = factorial(4);
 	int fac5 = factorial(5);
     	int numero;
-	
-	//Solicita un número entero al usuario
-	printf("Ingrese un número entero: ");
+	printf("Ingrese un número: ");
 	scanf("%d", &numero);
-
-	//Se llama a la función factorial y se envía el número ingresado por el usuario como argumento
 	int facn=factorial(numero);
-
-	//Imprime el resultado
-    	printf("4! = %d, 5! = %d, \nEl factorial de su número es: %d! = %d \n", fac4, fac5, numero, facn);
+    	printf("4! = %d, 5! = %d \nEl factorial del número ingresado corresponde a: %d! = %d \n", fac4, fac5, numero, facn);
     return 0;
 }
-
